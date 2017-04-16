c950f6181e02b836032a2469829384637c275f2d
Richard Lin <rgl001@ucsd.edu>
2017-04-16 16:33:53 -0700

    Cleanup queue

--- threadsafe_queue.h
+++ threadsafe_queue.h
@@ -1,31 +1,35 @@
+/**
+ * Source: http://codereview.stackexchange.com/questions/51235/udp-network-server-client-for-gaming-using-boost-asio
+ * License: MIT
+ */
+
 #include <mutex>
 #include <queue>
 #include <list>
 
-// Simple mutex-guarded queue
-template<typename T> class ThreadSafeQueue
-{
-private:
-	std::mutex mutex;
-	std::queue<T> queue;
+using namespace std;
+
+template<typename T> class ThreadSafeQueue {
 public:
-	void push(T value)
-	{
-		std::unique_lock<std::mutex> lock(mutex);
-		queue.push(value);
-	};
+  void push(T value) {
+    unique_lock<mutex> lock(m);
+    q.push(value);
+  };
 
-	T pop()
-	{
-		std::unique_lock<std::mutex> lock(mutex);
-		T value;
-		std::swap(value, queue.front());
-		queue.pop();
-		return value;
-	};
+  T pop() {
+    unique_lock<mutex> lock(m);
+    T value;
+    swap(value, q.front());
+    q.pop();
+    return value;
+  };
 
-	bool empty() {
-		std::unique_lock<std::mutex> lock(mutex);
-		return queue.empty();
-	}
+  bool empty() {
+    unique_lock<mutex> lock(m);
+    return q.empty();
+  }
+
+private:
+  mutex m;
+  queue<T> q;
 };
